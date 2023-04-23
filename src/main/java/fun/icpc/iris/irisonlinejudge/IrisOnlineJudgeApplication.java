497279f6ab7acc4b975d6a1d50fad28c27f71d8c
jinsuichen <ischenjinsui@gmail.com>
2023-04-23 21:26:42 +0800

    chore: Initialize SpringBoot

---
+++ src/main/java/fun/icpc/iris/irisonlinejudge/IrisOnlineJudgeApplication.java
@@ -0,0 +1,13 @@
+package fun.icpc.iris.irisonlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisOnlineJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisOnlineJudgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/icpc/iris/irisonlinejudge/IrisOnlineJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.icpc.iris.irisonlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisOnlineJudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
