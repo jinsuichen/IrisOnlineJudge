c1443bc1c22903c619328626d6ee0c946f560fcf
jinsuichen <ischenjinsui@gmail.com>
2023-04-24 18:37:19 +0800

    test: UserRepository

--- src/test/java/fun/icpc/iris/irisonlinejudge/user/UserRepositoryTests.java
+++ src/test/java/fun/icpc/iris/irisonlinejudge/user/UserRepositoryTests.java
@@ -1,15 +1,20 @@
 package fun.icpc.iris.irisonlinejudge.user;
 
+import fun.icpc.iris.irisonlinejudge.permission.Permission;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryTests {
 
     @Autowired
@@ -31,6 +36,54 @@ public class UserRepositoryTests {
         assertTrue(result.isPresent());
         assertEquals(user.getNickname(), result.get().getNickname());
         assertEquals(user.getHandle(), result.get().getHandle());
+    }
+
+    @Test
+    public void testFindByHandle() {
+        // Insert a user into the database
+        User user = new User();
+        user.setNickname("jack");
+        user.setHandle("jack2020");
+        entityManager.persist(user);
+
+        // Query the user from the database
+        Optional<User> result = userRepository.findByHandle(user.getHandle());
+        assertTrue(result.isPresent());
+        assertEquals(user.getNickname(), result.get().getNickname());
+        assertEquals(user.getHandle(), result.get().getHandle());
+    }
+
+
+    @Test
+    public void testFindByHandleNotExist() {
+        // Query the user from the database
+        Optional<User> result = userRepository.findByHandle("jack2020");
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testFindPermissionByHandle() {
+
+        // Create a permission
+        Set<Permission> permissions = new HashSet<>();
+        Permission permission = new Permission();
+        permission.setName("ADMIN");
+        entityManager.persist(permission);
+        permissions.add(permission);
+
+        // Insert a user into the database
+        User user = new User();
+        user.setNickname("jack");
+        user.setHandle("jack2020");
+        user.setPermissions(permissions);
+        entityManager.persist(user);
+
+        // Query the user from the database
+        Optional<User> result = userRepository.findByHandle(user.getHandle());
+        assertTrue(result.isPresent());
+        assertEquals(user.getNickname(), result.get().getNickname());
+        assertEquals(user.getHandle(), result.get().getHandle());
+        assertEquals(user.getPermissions(), result.get().getPermissions());
     }
 
 }
