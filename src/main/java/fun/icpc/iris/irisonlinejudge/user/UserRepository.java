a680f3edfa4b397656e9da112f18357351ee6de1
jinsuichen <ischenjinsui@gmail.com>
2023-04-24 18:39:14 +0800

    feat: New function findByHandle

--- src/main/java/fun/icpc/iris/irisonlinejudge/user/UserRepository.java
+++ src/main/java/fun/icpc/iris/irisonlinejudge/user/UserRepository.java
@@ -3,10 +3,11 @@ package fun.icpc.iris.irisonlinejudge.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
-
+    Optional<User> findByHandle(String handle);
 
 }
