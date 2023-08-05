a32a9badddc3dcc39b58eed4632b6f9d429e3f4c
Jinsui Chen <ischenjinsui@gmail.com>
2023-08-06 01:00:11 +0800

    refactor: delete auth component

--- src/main/java/fun/icpc/iris/irisonlinejudge/domain/user/User.java
+++ src/main/java/fun/icpc/iris/irisonlinejudge/domain/user/User.java
@@ -1,19 +1,6 @@
 package fun.icpc.iris.irisonlinejudge.domain.user;
 
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Table(indexes = {
         @Index(name = "idx_user_handle", columnList = "handle", unique = true)
 })
-public class User implements UserDetails {
+public class User {
 
     /**
      * The id of the user.
@@ -63,35 +50,4 @@ public class User implements UserDetails {
      */
     @Enumerated(EnumType.STRING)
     private Role role;
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return role.getAuthorities();
-    }
-
-    @Override
-    public String getUsername() {
-        return handle;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
